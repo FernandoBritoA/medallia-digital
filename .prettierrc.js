f311685853819ae95ef0b08ea6f0c3b244796ba5
brito-bc <fernando-brito@MacBook-Pro.local>
2022-10-31 18:58:31 -0500

    Project set up

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  jsxSingleQuote: true,
+  singleQuote: true,
+  semi: false,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  parser: 'babel-ts'
+}
